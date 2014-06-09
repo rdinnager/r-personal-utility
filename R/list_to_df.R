5e79e8af4bda87a18f04575edcd19658ad128f99
rdinnager <r.dinnage@gmail.com>
2014-06-09 23:42:04 +0800

    edited documentation

--- R/list_to_df.R
+++ R/list_to_df.R
@@ -4,7 +4,7 @@
 #' functions, particularly \code{\link[dplyr]{summarise}} and \code{\link[dplyr]{do}}.
 #' @param listfordf A list of R objects which you wish to convert for use in \code{dplyr}. Can be named or unnamed.
 #' @return a \code{dplyr} \code{\link[dplyr]{tbl_df}} object 
-#' @details This function was written by Hadley Wickham as a comment on \url{https://gist.github.com/aammd/9ae2f5cce9afd799bafb}
+#' @details The idea for this function came from Andrew Macdonald, and was written by Hadley Wickham as a comment on \url{https://gist.github.com/aammd/9ae2f5cce9afd799bafb}
 #' @export
 list_to_df <- function(listfordf){
   if(!is.list(listfordf)) stop("it should be a list")
